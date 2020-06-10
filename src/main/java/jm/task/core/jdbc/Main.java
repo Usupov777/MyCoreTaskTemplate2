a0a117c82d9053e0d8da5d654e64a4d9d2d9a92a
nikitanesterenkos@gmail.com <piramida741147>
2020-06-10 14:42:16 +0300

    Try

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Alan", "Wake", (byte) 42);
+        userService.saveUser("Marilyn", "Manson", (byte) 40);
+        userService.saveUser("Egor", "Letov", (byte) 43);
+        userService.saveUser("50", "Cent", (byte) 46);
+        List<User> users = userService.getAllUsers();
+        for(User user : users){
+            System.out.println(user);
+        }
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,33 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/mysql";
+    public static Connection connection;
+    public static Statement statement;
+
+    static {
+        try{
+            connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    static {
+        try{
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
 }
